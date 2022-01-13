3e0e6753b037a6eefe16d18634efd1850499d63e
Eric Trautman <trautmane@janelia.hhmi.org>
2022-01-13 14:10:05 -0500

    check for viewId issues when building transformed bounding box

--- src/main/java/net/preibisch/bigstitcher/spark/util/ViewUtil.java
+++ src/main/java/net/preibisch/bigstitcher/spark/util/ViewUtil.java
@@ -26,14 +26,29 @@ public class ViewUtil {
 
 	public static Interval getTransformedBoundingBox( final SpimData data,
 													  final ViewId viewId )
+			throws IllegalArgumentException
 	{
 		final ImgLoader imgLoader = data.getSequenceDescription().getImgLoader();
 		final SetupImgLoader<?> setupImgLoader = imgLoader.getSetupImgLoader(viewId.getViewSetupId());
+		if (setupImgLoader == null) {
+			throw new IllegalArgumentException(
+					"failed to find setupImgLoader for " + viewIdToString(viewId) + " in " + data);
+		}
 		final Dimensions dim = setupImgLoader.getImageSize(viewId.getTimePointId() );
 
 		final ViewRegistration reg = data.getViewRegistrations().getViewRegistration( viewId );
+		if (reg == null) {
+			throw new IllegalArgumentException(
+					"failed to find viewRegistration for " + viewIdToString(viewId) + " in " + data);
+		}
+
 		reg.updateModel();
 
 		return Intervals.largestContainedInterval( reg.getModel().estimateBounds( new FinalInterval( dim ) ) );
+	}
+
+	public static String viewIdToString(final ViewId viewId) {
+		return viewId == null ?
+			   null : "{\"setupId\": " + viewId.getViewSetupId() + ", \"timePointId\": " + viewId.getTimePointId() + "}";
 	}
 }
