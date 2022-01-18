da4dcad659282f91d89a9768a4fd1551ce37808f
Eric Trautman <trautmane@janelia.hhmi.org>
2022-01-18 11:29:02 -0500

    Remove shared SpimData2 instance and simply create a new copy for each task.  This creates many redundant instances but avoids multi-threaded initialization issues with the HDF5 loader (and possibly other loaders).

--- src/main/java/net/preibisch/bigstitcher/spark/util/Spark.java
+++ src/main/java/net/preibisch/bigstitcher/spark/util/Spark.java
@@ -1,12 +1,10 @@
 package net.preibisch.bigstitcher.spark.util;
 
 import java.util.List;
-import java.util.Map;
 
 import mpicbg.spim.data.SpimDataException;
 import mpicbg.spim.data.generic.sequence.BasicImgLoader;
 import mpicbg.spim.data.sequence.SequenceDescription;
-import mpicbg.spim.data.sequence.ViewDescription;
 import mpicbg.spim.data.sequence.ViewId;
 
 import org.apache.spark.SparkEnv;
@@ -50,73 +48,20 @@ public class Spark {
 												 final String xmlPath)
 			throws SpimDataException {
 
+		final SpimData2 data = new XmlIoSpimData2(clusterExt).load(xmlPath);
+		final SequenceDescription sequenceDescription = data.getSequenceDescription();
 
-		final SpimData2 data;
-		if (sparkJobSpimData2 == null) {
-			data = loadSpimData2(clusterExt, xmlPath);
-		} else {
-			validateSpimData2Location(clusterExt, xmlPath);
-			data = sparkJobSpimData2;
+		// set number of fetcher threads to 0 for spark usage
+		final BasicImgLoader imgLoader = sequenceDescription.getImgLoader();
+		if (imgLoader instanceof ViewerImgLoader) {
+			((ViewerImgLoader) imgLoader).setNumFetcherThreads(0);
 		}
 
-		LOG.info("getSparkJobSpimData2: returning {} for clusterExt={}, xmlPath={} on executorId={}",
+		LOG.info("getSparkJobSpimData2: loaded {} for clusterExt={}, xmlPath={} on executorId={}",
 				 data, clusterExt, xmlPath, getSparkExecutorId());
 
 		return data;
 	}
-
-	private static synchronized SpimData2 loadSpimData2(final String clusterExt,
-														final String xmlPath)
-			throws SpimDataException {
-
-		if (sparkJobSpimData2 == null) {
-
-			final SpimData2 data = new XmlIoSpimData2(clusterExt).load(xmlPath);
-			final SequenceDescription sequenceDescription = data.getSequenceDescription();
-
-			// set number of fetcher threads to 0 for spark usage
-			final BasicImgLoader imgLoader = sequenceDescription.getImgLoader();
-			if (imgLoader instanceof ViewerImgLoader) {
-				((ViewerImgLoader) imgLoader).setNumFetcherThreads(0);
-			}
-
-			// force lazy load of view data to prevent concurrent access issues later
-			final Map<ViewId, ViewDescription> viewDescriptions = sequenceDescription.getViewDescriptions();
-			sequenceDescription.getViewSetupsOrdered();
-
-			final ViewId firstViewId = viewDescriptions.keySet().stream().findFirst()
-					.orElseThrow(() -> new SpimDataException("cannot find first viewId in data with clusterExt " +
-															 clusterExt + " and xmlPath " + xmlPath));
-
-			final int firstSetupId = firstViewId.getViewSetupId();
-			imgLoader.getSetupImgLoader(firstSetupId);
-
-			sparkJobSpimData2ClusterExt = clusterExt;
-			sparkJobSpimData2XmlPath = xmlPath;
-			sparkJobSpimData2 = data;
-
-			LOG.info("loadSpimData2: loaded {} with firstSetupId {} for clusterExt={}, xmlPath={} on executorId={}",
-					 sparkJobSpimData2, firstSetupId, clusterExt, xmlPath, getSparkExecutorId());
-
-		} else {
-			validateSpimData2Location(clusterExt, xmlPath);
-		}
-
-		return sparkJobSpimData2;
-	}
-
-	private static void validateSpimData2Location(final String clusterExt,
-												  final String xmlPath) throws SpimDataException {
-		if (! (clusterExt.equals(sparkJobSpimData2ClusterExt) && xmlPath.equals(sparkJobSpimData2XmlPath))) {
-			throw new SpimDataException("attempted to load data with clusterExt " + clusterExt + " and xmlPath " +
-										xmlPath + " after data was already loaded with clusterExt " +
-										sparkJobSpimData2ClusterExt + " and xmlPath " + sparkJobSpimData2XmlPath);
-		}
-	}
-
-	private static String sparkJobSpimData2ClusterExt;
-	private static String sparkJobSpimData2XmlPath;
-	private static SpimData2 sparkJobSpimData2;
 
 	private static final Logger LOG = LoggerFactory.getLogger(Spark.class);
 
