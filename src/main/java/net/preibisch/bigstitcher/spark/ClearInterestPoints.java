457dc6f6c31ccf82299e3ef20156b7beeb21c0df
Stephan Preibisch <stephan.preibisch@gmx.de>
2024-03-05 09:49:13 -0500

    add utitily class to remove all interestpoints or correspondences from a project

---
+++ src/main/java/net/preibisch/bigstitcher/spark/ClearInterestPoints.java
@@ -0,0 +1,108 @@
+package net.preibisch.bigstitcher.spark;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.janelia.saalfeldlab.n5.N5FSWriter;
+
+import mpicbg.spim.data.sequence.ViewId;
+import net.preibisch.bigstitcher.spark.abstractcmdline.AbstractBasic;
+import net.preibisch.bigstitcher.spark.util.Import;
+import net.preibisch.mvrecon.fiji.spimdata.SpimData2;
+import net.preibisch.mvrecon.fiji.spimdata.XmlIoSpimData2;
+import net.preibisch.mvrecon.fiji.spimdata.interestpoints.InterestPoints;
+import net.preibisch.mvrecon.fiji.spimdata.interestpoints.InterestPointsN5;
+import net.preibisch.mvrecon.fiji.spimdata.interestpoints.ViewInterestPointLists;
+import net.preibisch.mvrecon.process.interestpointregistration.pairwise.constellation.grouping.Group;
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+
+public class ClearInterestPoints extends AbstractBasic
+{
+	@Option(names = { "--correspondencesOnly" }, description = "clear only corresponding interest points (default: false)")
+	private boolean correspondencesOnly = false;
+
+	private static final long serialVersionUID = -7892604354139919145L;
+
+	@Override
+	public Void call() throws Exception
+	{
+		final SpimData2 dataGlobal = this.loadSpimData2();
+
+		if ( dataGlobal == null )
+			return null;
+
+		final ArrayList< ViewId > viewIdsGlobal = Import.getViewIds( dataGlobal );
+		Collections.sort( viewIdsGlobal );
+
+		final Map<ViewId, ViewInterestPointLists> ips = dataGlobal.getViewInterestPoints().getViewInterestPoints();
+
+		// load all (shows debug stuff)
+		for ( final Entry<ViewId, ViewInterestPointLists> ip : ips.entrySet() )
+			for ( final Entry<String, InterestPoints> points : ip.getValue().getHashMap().entrySet() )
+			{
+				points.getValue().getInterestPointsCopy().size();
+				points.getValue().getCorrespondingInterestPointsCopy().size();
+			}
+
+		final String file = new File( dataGlobal.getBasePath().getAbsolutePath(), InterestPointsN5.baseN5 ).getAbsolutePath();
+
+		if ( correspondencesOnly)
+			System.out.println( "\tThe following correspondences will be removed in ('" + file + "'):");
+		else
+			System.out.println( "\tThe following interest points and correspondences will be removed in ('" + file + "'):");
+
+		// display all data
+		for ( final Entry<ViewId, ViewInterestPointLists> ip : ips.entrySet() )
+		{
+			System.out.println( Group.pvid( ip.getKey() ) + ":" );
+
+			for ( final Entry<String, InterestPoints> points : ip.getValue().getHashMap().entrySet() )
+				System.out.println( "\t'" + points.getKey() + "' " + points.getValue().getInterestPointsCopy().size() + " interest points, " + points.getValue().getCorrespondingInterestPointsCopy().size() + " correspondences." );
+		}
+
+		if ( !dryRun )
+		{
+			if ( correspondencesOnly )
+			{
+				for ( final Entry<ViewId, ViewInterestPointLists> ip : ips.entrySet() )
+				{
+					for ( final Entry<String, InterestPoints> points : ip.getValue().getHashMap().entrySet() )
+					{
+						System.out.println( "Clearing " + Group.pvid( ip.getKey() ) + ", '" + points.getKey() + "' ... " );
+
+						points.getValue().setCorrespondingInterestPoints( new ArrayList<>() );
+						points.getValue().saveCorrespondingInterestPoints( true );
+					}
+				}
+			}
+			else
+			{
+				System.out.println( "Saving XML (metadata only) ..." );
+	
+				dataGlobal.getViewInterestPoints().getViewInterestPoints().clear();
+				new XmlIoSpimData2( null ).save( dataGlobal, xmlPath );
+	
+				System.out.println( "Removing interest point directory '" + file + "' ... " );
+	
+				final N5FSWriter n5Writer = new N5FSWriter( file );
+				n5Writer.remove();
+				n5Writer.close();
+			}
+		}
+
+		System.out.println( "done" );
+
+		return null;
+	}
+
+	public static void main(final String... args)
+	{
+		System.out.println(Arrays.toString(args));
+		System.exit(new CommandLine(new ClearInterestPoints()).execute(args));
+	}
+
+}
