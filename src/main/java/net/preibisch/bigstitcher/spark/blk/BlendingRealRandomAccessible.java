943cb12190b2bc314afb7fc2f5e3aee3464d64fe
tpietzsch <tobias.pietzsch@gmail.com>
2024-03-19 12:34:53 +0100

    simplified BlendingRealRandomAccess

--- src/main/java/net/preibisch/bigstitcher/spark/blk/BlendingRealRandomAccessible.java
+++ src/main/java/net/preibisch/bigstitcher/spark/blk/BlendingRealRandomAccessible.java
@@ -76,7 +76,7 @@ public class BlendingRealRandomAccessible implements RealRandomAccessible< Float
 	@Override
 	public RealRandomAccess<FloatType> realRandomAccess()
 	{
-		return new BlendingRealRandomAccess( interval, border, blending );
+		return new BlendingRealRandomAccess2( interval, border, blending );
 	}
 
 	@Override
