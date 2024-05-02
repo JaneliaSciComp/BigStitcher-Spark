08170d71dbf3129b156938d92cecbe678605a2ce
Stephan Preibisch <stephan.preibisch@gmx.de>
2024-05-01 20:16:17 -0400

    add code for on-the-fly background removal

---
+++ src/main/java/net/preibisch/bigstitcher/spark/detection/LazyBackgroundSubtract.java
@@ -0,0 +1,173 @@
+package net.preibisch.bigstitcher.spark.detection;
+
+import java.io.File;
+import java.util.function.Consumer;
+
+import ij.ImageJ;
+import ij.Prefs;
+import ij.plugin.filter.RankFilters;
+import ij.process.FloatProcessor;
+import net.imglib2.Cursor;
+import net.imglib2.FinalInterval;
+import net.imglib2.Interval;
+import net.imglib2.RandomAccessible;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.basictypeaccess.AccessFlags;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.util.Intervals;
+import net.imglib2.view.Views;
+import net.preibisch.legacy.io.IOFunctions;
+import util.Lazy;
+
+public class LazyBackgroundSubtract<T extends RealType<T> & NativeType<T>> implements Consumer<RandomAccessibleInterval<T>>
+{
+	final T type;
+	final private int radiusXY;
+	final long[] globalMin;
+	final private RandomAccessible<T> source;
+	final int n;
+
+	public LazyBackgroundSubtract(
+			final long[] min,
+			RandomAccessible<T> source,
+			final T type,
+			final int radiusXY )
+	{
+		while ( source.numDimensions() < 3 )
+			source = Views.addDimension( source );
+
+		if ( source.numDimensions() > 3 )
+			throw new RuntimeException( "Currently only max 3 dimensions supported." );
+
+		this.source = source;
+		this.globalMin = min;
+		this.type = type;
+		this.radiusXY = radiusXY;
+		this.n = 3;
+	}
+
+	// Note: the output RAI typically sits at 0,0...0 because it usually is a CachedCellImage
+	// (but the actual interval to process in many blocks sits somewhere else) 
+	@Override
+	public void accept( final RandomAccessibleInterval<T> outputZeroMin )
+	{
+		final RandomAccessibleInterval<T> output = Views.translate( outputZeroMin, globalMin );
+
+		// copy each slice to a FloatProcessor, size + kernelradius * 2
+		final Interval iterateInterval = Intervals.expand( output, new long[] { radiusXY, radiusXY, 0 } );
+		final FloatProcessor fp = new FloatProcessor( (int)iterateInterval.dimension( 0 ), (int)iterateInterval.dimension( 1 ) );
+		final float[] pixelsMedian = (float[])fp.getPixels();
+		final float[] pixels = new float[ pixelsMedian.length ];
+
+		// just a wrapper around the arrays
+		final Img<FloatType> imgMedian = ArrayImgs.floats( pixelsMedian, iterateInterval.dimension( 0 ), iterateInterval.dimension( 1 ) );
+		final Img<FloatType> img = ArrayImgs.floats( pixels, iterateInterval.dimension( 0 ), iterateInterval.dimension( 1 ) );
+
+		// the interval in the FloatProcessor to copy into the output
+		final Interval cropInterval = Intervals.translate( new FinalInterval( output.dimension( 0 ), output.dimension( 1 ) ), new long[] { radiusXY, radiusXY } );
+		final RandomAccessibleInterval< FloatType > imgMedianCrop = Views.interval( imgMedian, cropInterval );
+		final RandomAccessibleInterval< FloatType > imgCrop = Views.interval( img, cropInterval );
+
+		/*
+		// init single-threaded
+		final RankFilters rf;
+		synchronized ( this )
+		{
+			final int numThreads = Prefs.getThreads();
+			Prefs.setThreads( 1 );
+			rf = new RankFilters();
+			Prefs.setThreads( numThreads );
+			rf.setNPasses( 0 ); // suppress progress bar
+		}
+		*/
+		final RankFilters rf = new RankFilters();
+		rf.setNPasses( 0 ); // suppress progress bar
+
+		// for each z slice do
+		for ( long z = iterateInterval.min( 2 ); z <= iterateInterval.max( 2 ); ++z )
+		{
+			int i = 0;
+			for ( final T t : Views.flatIterable( Views.hyperSlice( Views.interval( source, iterateInterval ), 2, z ) ) )
+				pixelsMedian[ i ] = pixels[ i++ ] = t.getRealFloat();
+
+			// run median
+			rf.rank(fp, radiusXY, RankFilters.MEDIAN );
+
+			//ImageJFunctions.show( imgMedian );
+			//ImageJFunctions.show( img );
+
+			// subtract only the center
+			final Cursor< T > out = Views.flatIterable( Views.hyperSlice( output, 2, z ) ).cursor();
+			final Cursor< FloatType > median = Views.flatIterable( imgMedianCrop ).cursor();
+			final Cursor< FloatType > image = Views.flatIterable( imgCrop ).cursor();
+
+			while ( out.hasNext() )
+			{
+				final float m = median.next().get();
+				final float v = image.next().get();
+
+				out.next().setReal( v / m );
+			}
+
+			//ImageJFunctions.show( output );
+			//SimpleMultiThreading.threadHaltUnClean();
+		}
+	}
+
+	public static final <T extends RealType<T> & NativeType<T>> RandomAccessibleInterval<T> init(
+			final RandomAccessible< T > input,
+			final Interval processingInterval,
+			final T type,
+			final int radiusXY,
+			final int[] blockSize )
+	{
+		final long[] min = processingInterval.minAsLongArray();
+
+		final LazyBackgroundSubtract< T > lazyBG =
+				new LazyBackgroundSubtract<>(
+						min,
+						input,
+						type.createVariable(),
+						radiusXY );
+
+		final RandomAccessibleInterval<T> gauss =
+				Views.translate(
+						Lazy.process(
+								processingInterval,
+								blockSize,
+								type.createVariable(),
+								AccessFlags.setOf(),
+								lazyBG ),
+						min );
+
+		return gauss;
+	}
+
+	public static void main( String[] args )
+	{
+		new ImageJ();
+
+		final RandomAccessibleInterval< FloatType > raw =
+				//IOFunctions.openAs32BitArrayImg( new File( "/Users/preibischs/Documents/Microscopy/SPIM/HisYFP-SPIM/spim_TL18_Angle0.tif"));
+				IOFunctions.openAs32BitArrayImg( new File( "/Users/preibischs/Documents/Janelia/Projects/BigStitcher/Allen/tile_x_0002_y_0000_z_0000_ch_488.zarr-s4.tif"));
+
+		//RandomAccessibleInterval< FloatType > inputCropped = Views.interval( Views.extendMirrorDouble( raw ), Intervals.expand( raw, new long[] {-200, -200, -30}) );
+		RandomAccessibleInterval< FloatType > inputCropped = Views.interval( Views.extendMirrorDouble( raw ), Intervals.expand( raw, new long[] {-20, -20, -3}) );
+
+		ImageJFunctions.show( inputCropped );
+
+		RandomAccessibleInterval<FloatType> bgCorrected = LazyBackgroundSubtract.init(
+				inputCropped,
+				new FinalInterval(inputCropped),
+				new FloatType(),
+				20,
+				new int[] {128, 128, 1} );
+
+		ImageJFunctions.show( bgCorrected );
+	}
+}
