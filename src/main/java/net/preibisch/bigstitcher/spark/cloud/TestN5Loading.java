db94f816100f3e68f1c4a672595144c5c93a539e
Stephan Preibisch <stephan.preibisch@gmx.de>
2024-08-02 14:41:03 -0400

    tested s3 access

--- src/main/java/net/preibisch/bigstitcher/spark/cloud/TestN5Loading.java
+++ src/main/java/net/preibisch/bigstitcher/spark/cloud/TestN5Loading.java
@@ -156,8 +156,8 @@ public class TestN5Loading
 		//CloudUtil.parseCloudLink( "s3://janelia-bigstitcher-spark/Stitching/dataset.xml" );
 
 		//testLoadInterestPoints();
-		//testBigStitcherGUI( "s3://janelia-bigstitcher-spark/Stitching/dataset.xml" );
-		testBigStitcherGUI( "/Users/preibischs/Documents/Janelia/Projects/BigStitcher/Allen/bigstitcher_emr_708369_2024-04-23_06-52-14_2.xml" );
+		testBigStitcherGUI( "s3://janelia-bigstitcher-spark/Stitching/dataset.xml" );
+		//testBigStitcherGUI( "/Users/preibischs/Documents/Janelia/Projects/BigStitcher/Allen/bigstitcher_emr_708369_2024-04-23_06-52-14_2.xml" );
 		//s3://aind-open-data/exaSPIM_708369_2024-04-08_15-20-36_flatfield-correction_2024-04-16_20-33-12/SPIM.ome.zarr
 		//testBDV();
 		//testInterestPoints();
