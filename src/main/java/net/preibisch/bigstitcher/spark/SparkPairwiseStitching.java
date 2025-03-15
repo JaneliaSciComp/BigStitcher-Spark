3e4b56312cf3af0412dd3e4acce46663a5b81d34
Stephan Preibisch <stephan.preibisch@gmx.de>
2025-03-15 10:43:37 -0400

    add debug output for https://github.com/JaneliaSciComp/BigStitcher-Spark/issues/43

--- src/main/java/net/preibisch/bigstitcher/spark/SparkPairwiseStitching.java
+++ src/main/java/net/preibisch/bigstitcher/spark/SparkPairwiseStitching.java
@@ -42,6 +42,7 @@ import mpicbg.spim.data.sequence.Channel;
 import mpicbg.spim.data.sequence.Illumination;
 import mpicbg.spim.data.sequence.Tile;
 import mpicbg.spim.data.sequence.TimePoint;
+import mpicbg.spim.data.sequence.ViewDescription;
 import mpicbg.spim.data.sequence.ViewId;
 import net.imglib2.RealInterval;
 import net.imglib2.realtransform.AffineGet;
@@ -198,6 +199,29 @@ public class SparkPairwiseStitching extends AbstractSelectableViews
 			boolean nonTranslationsEqual = TransformTools.nonTranslationsEqual( vrs.getViewRegistration( firstVdA ), vrs.getViewRegistration( firstVdB ) );
 
 			final Pair<Pair< AffineGet, Double >, RealInterval> result;
+
+			// debug code for https://github.com/JaneliaSciComp/BigStitcher-Spark/issues/43
+			System.out.println( "\nDebugging ViewDescriptions ... " );
+			for ( final ViewId v : pair.getA() )
+			{
+				System.out.println( "ViewId=" + Group.pvid( v ) );
+				ViewDescription vd = data.getSequenceDescription().getViewDescription( v );
+				System.out.println( "ViewDescription=" + Group.pvid( vd ) );
+				System.out.println( "ViewSetup=" + vd.getViewSetup() );
+				System.out.println( "ViewSetup.getAttributes()=" + vd.getViewSetup().getAttributes() );
+				System.out.println( "ViewSetup.getAttributes().size()=" + vd.getViewSetup().getAttributes().size() );
+			}
+
+			for ( final ViewId v : pair.getB() )
+			{
+				System.out.println( "ViewId=" + Group.pvid( v ) );
+				ViewDescription vd = data.getSequenceDescription().getViewDescription( v );
+				System.out.println( "ViewDescription=" + Group.pvid( vd ) );
+				System.out.println( "ViewSetup=" + vd.getViewSetup() );
+				System.out.println( "ViewSetup.getAttributes()=" + vd.getViewSetup().getAttributes() );
+				System.out.println( "ViewSetup.getAttributes().size()=" + vd.getViewSetup().getAttributes().size() );
+			}
+
 
 			if (nonTranslationsEqual)
 			{
