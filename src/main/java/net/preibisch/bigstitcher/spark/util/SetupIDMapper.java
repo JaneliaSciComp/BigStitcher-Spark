428d296b46f3a705dfcd33be79f957c08fc2d347
Stephan Preibisch <stephan.preibisch@gmx.de>
2025-08-28 16:27:13 -0400

    moved mapping into a helper interface/class that we can call in AffineFusion

--- src/main/java/net/preibisch/bigstitcher/spark/util/SetupIDMapper.java
+++ src/main/java/net/preibisch/bigstitcher/spark/util/SetupIDMapper.java
@@ -1,71 +1,18 @@
-package net.preibisch.bigstitcher.spark;
+package net.preibisch.bigstitcher.spark.util;
 
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
 import mpicbg.spim.data.SpimDataException;
-import mpicbg.spim.data.sequence.ViewId;
 import mpicbg.spim.data.sequence.ViewSetup;
-import net.preibisch.bigstitcher.spark.abstractcmdline.AbstractBasic;
-import net.preibisch.bigstitcher.spark.util.Import;
 import net.preibisch.mvrecon.fiji.spimdata.SpimData2;
-import picocli.CommandLine;
-import picocli.CommandLine.Option;
-import util.URITools;
 
-public class ResortViewSetupIDs extends AbstractBasic
+public interface SetupIDMapper
 {
-	private static final long serialVersionUID = -8449058590348222341L;
-
-	@Option(names = { "-xo", "--xmlout" }, description = "path to the output BigStitcher xml, e.g. /home/project-n5.xml or s3://myBucket/dataset.xml (default: overwrite input)")
-	private String xmlOutURIString = null;
-
-	private URI xmlOutURI = null;
-
-	@Override
-	public Void call() throws Exception
-	{
-		this.setRegion();
-
-		// if we want to display the result, we need to set the fetcher threads to anything but 0
-		final SpimData2 data = this.loadSpimData2();
-
-		if ( data == null )
-			throw new IllegalArgumentException( "Couldn't load SpimData XML project." );
-
-		final ArrayList< ViewId > viewIds = Import.getViewIds( data );
-
-		if ( viewIds == null || viewIds.size() == 0 )
-			return null;
-
-		if ( xmlOutURIString == null )
-			xmlOutURI = xmlURI;
-		else
-			xmlOutURI = URITools.toURI( xmlOutURIString );
-
-		System.out.println( "xmlout: " + xmlOutURI );
-
-		// we build a map from old to new ViewSetupId
-		final SetupIDMapper mapper = new KellerMirrorScopeMapper( 1120/28, 28 );
-		final Map< Integer, Integer > map = mapper.map( data );
-
-		return null;
-	}
-
-	public static void main(final String... args) throws SpimDataException
-	{
-		System.out.println(Arrays.toString(args));
-		System.exit(new CommandLine(new ResortViewSetupIDs()).execute(args));
-	}
-
-	public static interface SetupIDMapper
-	{
-		Map< Integer, Integer > map( final SpimData2 data );
-	}
+	Map< Integer, Integer > map( final SpimData2 data );
 
 	public static class KellerMirrorScopeMapper implements SetupIDMapper
 	{
@@ -75,6 +22,14 @@ public class ResortViewSetupIDs extends AbstractBasic
 		{
 			this.rowCount = rowCount;
 			this.columnCount = columnCount;
+		}
+
+		public static void main( String args[] ) throws SpimDataException
+		{
+			final SpimData2 data = Spark.getSparkJobSpimData2( URI.create( "file:/Users/preibischs/Downloads/dataset_full.xml") );
+
+			// we build a map from old to new ViewSetupId
+			final Map< Integer, Integer > map = new KellerMirrorScopeMapper( 1120/28, 28 ).map( data );
 		}
 
 		@Override
