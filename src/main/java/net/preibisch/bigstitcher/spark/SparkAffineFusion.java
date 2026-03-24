edb65d66a7ecfb2e5c6bb97a88c8afe7c22df1cd
indecisiveuser <lila.schweinfurth@gmail.com>
2026-03-24 14:37:43 -0400

    fix: only load overlapping view, not all views in fusion

--- src/main/java/net/preibisch/bigstitcher/spark/SparkAffineFusion.java
+++ src/main/java/net/preibisch/bigstitcher/spark/SparkAffineFusion.java
@@ -602,7 +602,7 @@ public class SparkAffineFusion extends AbstractInfrastructure implements Callabl
 							blockSupplier = BlkAffineFusion.initWithIntensityCoefficients(
 									conv,
 									dataLocal.getSequenceDescription().getImgLoader(),
-									viewIds,
+									overlappingViews,
 									registrations,
 									dataLocal.getSequenceDescription().getViewDescriptions(),
 									fusionType,//fusion.getFusionType(),
